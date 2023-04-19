4c94ec1af01aae062a6959f5ccecfa5d3359bf80
dothedro <drooooofficial@gmail.com>
2023-04-19 12:40:15 +0800

    updated: CV link

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -21,7 +21,7 @@
             <p class="text-gray-100 text-lg text-center font-normal">Since I was a kid I like to solve puzzles and play with blocks turns out programming is almost the same. It needs different kinds of approaches and the use of different blocks to solve a specific problem.</p>
             <p class="text-gray-100 text-lg text-center font-normal">I started programming about 7 years ago mainly in Arduino but then I decided to go into web development.</p>
             <p class="text-gray-100 text-lg text-center font-normal">When I learn something new, I like to write it down in my own words and how I understand it so I don't forget it.</p>
-            <a class="py-3 px-5 bg-blue-700 w-max rounded text-gray-100 duration-500 lg:hover:scale-110" href="https://drive.google.com/file/d/18NwabgkgIGGRSblg62FQ3AB3A24fDMNd/view" target="_blank">Download CV</a>
+            <a class="py-3 px-5 bg-blue-700 w-max rounded text-gray-100 duration-500 lg:hover:scale-110" href="https://drive.google.com/file/d/1B8VG_x2h47fuGiH5w9GHMMNDAHXQiDXt/view?usp=sharing" target="_blank">Download CV</a>
         </div>
     </div>
 </template>
